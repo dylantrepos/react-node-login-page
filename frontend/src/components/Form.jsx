1e5e01d1a480de9951149857d258f8c67f8d0d58
Dylan Trepos <dylan.trepos@gmail.com>
2022-02-18 14:56:35 +0100

    Set validation requirements to the form

--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,36 +1,52 @@
+import { useState } from 'react';
 import { appendErrors, useForm } from 'react-hook-form';
 
 export default function Form () {
 
     const { register, handleSubmit, watch, formState: {errors} } = useForm();
+    const [form, setForm] = useState({});
+    const onSubmit = data => {
+        setForm({data})
+        console.log(data)
+    }
     
     // console.log(watch("firstname"));
 
-    return <form onSubmit={handleSubmit((data) => console.log(data))}>
+    return <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="form" >
                     <div className="form-group">
                         <label htmlFor="username">Username</label>
-                        <input {...register("username", { required: "This is required." })} placeholder="Username..." />
+                        <input {...register("username", { required: "This is required.", 
+                            minLength: {  
+                                value: 5,
+                                message: 'Username must be at least 5 characters.'
+                            }})} placeholder="Username..." />
                         <p className='errors'>{errors.username?.message}</p>
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input {...register("password", { required: "This is required.", minLength: {
-                            value: 4,
-                            message: "Min length is 4"
+                        <input type="password" {...register("password", { required: "This is required.", minLength: {
+                            value: 8,
+                            message: "Min length is 8"
+                        }, pattern: {
+                            value: /^(((?=.*[a-z])(?=.*[A-Z]))((?=.*[a-z])(?=.*[0-9]))((?=.*[A-Z])(?=.*[0-9]))(?=.*[!@#$%^&*]))/,
+                            message: "Password must contains at least 1 lowercase, 1 uppercase, 1 numeric, 1 special character"
                         }})} placeholder="Password..." />
                         <p className='errors'>{errors.password?.message}</p>
                     </div>
                     <div className="form-group">
                         <label htmlFor="name">Name</label>
-                        <input {...register("name", { required: "This is required." })} placeholder="Name..." />
+                        <input {...register("name", { required: "This is required.", pattern: { 
+                            value: /^[a-zA-Z]+$/gm,
+                            message: "Name can only contains letters (a-z)."
+                        }})} placeholder="Name..." />
                         <p className='errors'>{errors.name?.message}</p>
                     </div>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input {...register("email", { required: "This is required.", pattern: {
-                                value: /@[.]/g,
-                                message: 'Please enter a valid email address' // JS only: <p>error message</p> TS only support string
+                        <input type="email" {...register("email", { required: "This is required.", pattern: {
+                                value: /\w*@\w*\.[a-z]*/,
+                                message: 'Please enter a valid email address'
                                 } })} placeholder="Email..." />
                         <p className='errors'>{errors.email?.message}</p>
                     </div>
