99c1f007b42663b251c025d4dd9631aff47b3578
Dylan Trepos <dylan.trepos@gmail.com>
2022-03-02 15:50:09 +0100

    change user info

--- backend/models/dbConfig.js
+++ backend/models/dbConfig.js
@@ -6,12 +6,8 @@ const host = process.env.DB_HOST;
 const port = process.env.DB_PORT;
 const db = process.env.DB_DATABASE;
 
-/**
- * Add your MongoDB informations here
- * local : "mongodb://localhost:27017/node-login"
- * online : "mongodb://cluster0-shard-00-02.l9hpw.mongodb.net:27017/node-login"
- */
-mongoose.connect(`mongodb+srv://dtdbmgdb:xCQ1MHr8WbiQdXG4@cluster0.l9hpw.mongodb.net/node-login?retryWrites=true&w=majority`, {
+
+mongoose.connect(`your-db-config`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).
